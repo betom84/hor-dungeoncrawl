1b8ca4aad62438b5aa4bd2c517b873916e9c385e
Tom Beyer <tom@betom.de>
2023-11-26 16:40:57 +0100

    Compose Dungeons Denizens (Chapter 6)

--- src/map.rs
+++ src/map.rs
@@ -19,29 +19,6 @@ impl Map {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(0);
-
-        for y in camera.top_y..camera.bottom_y {
-            for x in camera.left_x..camera.right_x {
-                let tile_idx = map_idx(x, y);
-                if tile_idx.is_none() {
-                    continue;
-                }
-
-                if let Some(tile) = self.tiles.get(tile_idx.expect("can't be none")) {
-                    let x = x - camera.left_x;
-                    let y = y - camera.top_y;
-
-                    match tile { 
-                    TileType::Wall => ctx.set(x, y, GREEN, BLACK, to_cp437('#')),
-                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.')),
-                }
-                }
-            }
-        }
-    }
-
     pub fn can_enter_tile(&self, p: Point) -> bool {
         if let Some(idx) = map_idx(p.x, p.y) {
             if let Some(&tile) = self.tiles.get(idx) {
